9cef3b7b22a0e63c4ef2038ec4bd1170cbaede64
MaxIvanyshen <maxivanyshen@gmail.com>
2022-06-14 21:52:26 +0300

    finish the bot

---
+++ src/main/java/Ripper.java
@@ -0,0 +1,128 @@
+import org.jsoup.Connection;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import javax.print.DocFlavor;
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Ripper {
+
+    private static Document doc;
+
+    private static void connect(String url) {
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (SocketTimeoutException e) {
+            return;
+        } catch (IOException e) {
+            return;
+        }
+    }
+
+    private static void connect(String url, int pageNumber) {
+        try {
+            doc = Jsoup.connect(url).get();
+            if(pageNumber > 1) {
+                Elements els = doc.getElementsByAttributeValue("aria-label", "Page " + pageNumber);
+                String newUrl = "https://www.google.com" + els.get(0).attr("href");
+                doc = Jsoup.connect(newUrl).get();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String[] ripWebsiteAddresses(String url, int pageNumber) {
+
+        connect(url, pageNumber);
+
+        Elements addresses = doc.getElementsByTag("a");
+        ArrayList<String> linkList = new ArrayList<>();
+        for(Element link : addresses) {
+            String s = link.attr("href");
+            boolean notNeededLink = s.startsWith("https://www.google.com") || s.startsWith("/search") || s.startsWith("https://support")
+                    || s.startsWith("https://policies")|| s.equals("#") || s.equals("");
+            if(notNeededLink)
+                continue;
+            linkList.add(s);
+        }
+        return linkList.toArray(new String[0]);
+    }
+
+    public static String[] ripCompanyNames(String url, int pageNumber) {
+
+        connect(url, pageNumber);
+
+        ArrayList<String> websiteList = new ArrayList<>();
+
+        Elements s = doc.getElementsByAttributeValue("role", "heading");
+        for(Element e : s) {
+            if(e.child(0).ownText().equals(""))
+                continue;
+            websiteList.add(e.child(0).ownText());
+        }
+
+        return websiteList.toArray(new String[0]);
+
+    }
+
+    public static ArrayList<String> ripEmails(String url) {
+
+        ArrayList<String> emails = new ArrayList<>();
+
+        addToCollectionFoundEmails(url, emails);
+        addToCollectionFoundEmails(getContactPage(url), emails);
+
+        return emails;
+    }
+
+    private static void addToCollectionFoundEmails(String url, ArrayList<String> emails) {
+        try {
+            connect(url);
+        } catch (IllegalArgumentException e) {}
+
+        Pattern p = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+");
+        Matcher matcher = p.matcher(doc.text());
+        while (matcher.find()) {
+            String s = matcher.group();
+            if(s.endsWith("."))
+                s = s.substring(0, s.length() - 2);
+            if(!emails.contains(s))
+                emails.add(s);
+        }
+    }
+
+    private static String getContactPage(String url) {
+        String contactPageLink = "";
+
+        connect(url);
+
+        Elements links = doc.select("a");
+
+        for(Element link : links) {
+            boolean needed = link.ownText().equalsIgnoreCase("contact") || link.ownText().equalsIgnoreCase("contact us") ||
+                    link.ownText().equalsIgnoreCase("kontakt");
+            if(needed) {
+                String text = link.ownText();
+                if(link.attr("href").startsWith("https://") || link.attr("href").startsWith("http://")) {
+                    contactPageLink = link.attr("href");
+                } else {
+                    contactPageLink = url + (link.attr("href").replaceFirst("/", ""));
+                }
+            }
+        }
+
+        return contactPageLink;
+    }
+
+}
---
+++ src/test/java/RipperTest.java
@@ -0,0 +1,100 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.net.SocketTimeoutException;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RipperTest {
+
+
+    @Test
+    public void testRippingWebsiteAddresses() {
+        String url = "https://www.google.com/search?q=architecture+firms+switzerland&biw=1886&bih=920&sz=0&tbm=lcl&sxsrf=ALiCzsbMAMD-NcwAi_ZIbxmfCW4gW1iSSw%3A1654636900534&ei=ZMGfYtqRIJTzrgTuvZjwCQ&oq=architecture+firms+switzerland&gs_l=psy-ab.3...0.0.0.1663.0.0.0.0.0.0.0.0..0.0....0...1c..64.psy-ab..0.0.0....0.wZ-gvkTbta8#rlfi=hd:;si:;mv:[[47.6596465,9.115573999999999],[45.915672199999996,5.960416899999999]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3,lf:1";
+        String[] result = Ripper.ripWebsiteAddresses(url, 1);
+        assertEquals("http://www.herzogdemeuron.com/", result[0]);
+    }
+
+
+    @Test
+    public void testRippingWebsiteAddresses_secondPage() {
+        String url = "https://www.google.com/search?q=architecture+firms+greece&sz=0&biw=1886&bih=920&tbm=lcl&sxsrf=ALiCzsYtN31AiZ3gkoRlVaH6FJ1I18qVpg%3A1654951169224&ei=AY2kYoamDYbSrgSagoGACg&oq=architecture+firms+greece&gs_l=psy-ab.3...0.0.0.3151.0.0.0.0.0.0.0.0..0.0....0...1c..64.psy-ab..0.0.0....0.aisx6Q6WoF0#rlfi=hd:;si:;mv:[[40.7766945,25.564568400000002],[37.2570815,21.5141685]];start:0";
+        String[] result = Ripper.ripWebsiteAddresses(url, 2);
+        assertEquals("http://stirixis.com/", result[0]);
+    }
+
+    @Test
+    public void testRippingWebsiteAddresses_thirdPage() {
+        String url = "https://www.google.com/search?q=architecture+firms+greece&sz=0&biw=1886&bih=920&tbm=lcl&sxsrf=ALiCzsYtN31AiZ3gkoRlVaH6FJ1I18qVpg%3A1654951169224&ei=AY2kYoamDYbSrgSagoGACg&oq=architecture+firms+greece&gs_l=psy-ab.3...0.0.0.3151.0.0.0.0.0.0.0.0..0.0....0...1c..64.psy-ab..0.0.0....0.aisx6Q6WoF0#rlfi=hd:;si:;mv:[[40.7766945,25.564568400000002],[37.2570815,21.5141685]];start:0";
+        String[] result = Ripper.ripWebsiteAddresses(url, 3);
+        assertEquals("http://www.rctech.gr/", result[0]);
+    }
+
+    @Test
+    public void testRippingCompanyNames() {
+        String url = "https://www.google.com/search?q=architecture+firms+greece&sz=0&biw=1886&bih=920&tbm=lcl&sxsrf=ALiCzsYtN31AiZ3gkoRlVaH6FJ1I18qVpg%3A1654951169224&ei=AY2kYoamDYbSrgSagoGACg&oq=architecture+firms+greece&gs_l=psy-ab.3...0.0.0.3151.0.0.0.0.0.0.0.0..0.0....0...1c..64.psy-ab..0.0.0....0.aisx6Q6WoF0#rlfi=hd:;si:;mv:[[40.7766945,25.564568400000002],[37.2570815,21.5141685]];start:0";
+        String[] result = Ripper.ripCompanyNames(url, 2);
+        assertEquals("Stirixis Group", result[0]);
+        assertEquals("Parti Architecture", result[1]);
+    }
+
+
+    @Test
+    public void testRippingEmailFromWebsite() {
+        String url = "https://www.evolution-design.info/";
+        ArrayList<String> result = Ripper.ripEmails(url);
+        assertTrue(result.contains("zurich@evolution-design.info"));
+    }
+
+    @Test
+    public void testWritingToCSV() throws IOException {
+        String url = "http://norellrodhe.se/";
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        map.put(url, Ripper.ripEmails(url));
+        File f = new File("./test.csv");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(f));
+        writer.write(url + ",");
+        ArrayList<String> emails = map.get(url);
+
+        for(int i = 0; i < 3; i++) {
+            if(i < emails.size())
+                writer.write(emails.get(i));
+            if(i != 2)
+                writer.write(",");
+        }
+
+        writer.write("\n");
+        writer.close();
+
+        Scanner sc = new Scanner(f);
+        String s = sc.nextLine();
+        assertFalse(sc.hasNextLine());
+
+        f.delete();
+    }
+
+    @Test
+    @Disabled
+    public void finalTest() {
+//        String url = "https://www.google.com/search?q=architecture+firms+switzerland&biw=1886&bih=920&sz=0&tbm=lcl&sxsrf=ALiCzsbMAMD-NcwAi_ZIbxmfCW4gW1iSSw%3A1654636900534&ei=ZMGfYtqRIJTzrgTuvZjwCQ&oq=architecture+firms+switzerland&gs_l=psy-ab.3...0.0.0.1663.0.0.0.0.0.0.0.0..0.0....0...1c..64.psy-ab..0.0.0....0.wZ-gvkTbta8#rlfi=hd:;si:;mv:[[47.6596465,9.115573999999999],[45.915672199999996,5.960416899999999]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3,lf:1";
+        String url = "https://www.google.com/search?q=architecture+firms+greece&sz=0&biw=1886&bih=920&tbm=lcl&sxsrf=ALiCzsYtN31AiZ3gkoRlVaH6FJ1I18qVpg%3A1654951169224&ei=AY2kYoamDYbSrgSagoGACg&oq=architecture+firms+greece&gs_l=psy-ab.3...0.0.0.3151.0.0.0.0.0.0.0.0..0.0....0...1c..64.psy-ab..0.0.0....0.aisx6Q6WoF0#rlfi=hd:;si:;mv:[[40.7766945,25.564568400000002],[37.2570815,21.5141685]];start:0";
+        String[] websites = Ripper.ripWebsiteAddresses(url, 1);
+        HashMap<String, ArrayList<String>> emailsMap = new HashMap<>();
+        ArrayList<String> websitesList = new ArrayList<>();
+        for(String website : websites) {
+            websitesList.add(website);
+            ArrayList<String> emailsResult = Ripper.ripEmails(website);
+            emailsMap.put(website, emailsResult);
+        }
+
+        for (Map.Entry<String,ArrayList<String>> entry : emailsMap.entrySet())
+            System.out.println("Website = " + entry.getKey() +
+                    "\n Emails = " + entry.getValue());
+
+        System.out.println(emailsMap.size());
+
+        assertFalse(emailsMap.isEmpty());
+    }
+}
